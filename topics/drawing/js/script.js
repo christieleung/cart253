21cb97b9d93c8336abc79db5ab977fe323e61a33
christie <christie.leung83@gmail.com>
2025-09-11 08:34:28 -0400

    started on drawing module

---
+++ topics/drawing/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Drawing Module
+ * Christie Leung
+ * 
+ * Practing and learning drawing functions in p5
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(640, 640);
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
