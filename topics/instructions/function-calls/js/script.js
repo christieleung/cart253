dfa3d4aa75162031483d42fd3be64f8ce74a9e65
christie <christie.leung83@gmail.com>
2025-09-11 08:27:30 -0400

    added comments to the Blank Page project

--- topics/instructions/function-calls/js/script.js
+++ topics/instructions/function-calls/js/script.js
@@ -1,22 +1,32 @@
 /**
- * Function Calls
+ * The Blank Page
  * Christie Leung
  * 
- * Working on the function calls examples.
+ * An exploration of the existenential angst of a novelist
+ * who must sit down at their pink desk and confront the 
+ * abyss that is a blank page of paper.
+ * 
+ * The program is non-interactive to convey the inability
+ * to get started on the project. Try typing! Just try it!
  */
 
 "use strict";
 
-
+/**
+ * Creates the canvas for our masterpiece
+ */
 function setup() {
-    // Will happen once at the beginning of the program
+    //  Create the canvas at a standard resolution
     createCanvas(640, 480);
 }
 
+
+/**
+ * Draws the writer's desktop and a blank piece of paper
+ */
 function draw() {
-    // Will happen every frame that the program runs
+    // The pink desktop
     background(255, 100, 100);
-    // RGB colour model (between 0 and 255)
+    // The blank piece of paper
     rect(200, 80, 240, 320);
-    // Function to draw rectangles (x, y, width, height)
-}
+} 
