814fb13475b07a119d03cc031afc69c5a66030ec
christie <christie.leung83@gmail.com>
2025-09-18 07:07:52 -0400

    started the movement module

---
+++ topics/variables/movement/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Movement
+ * Christie Leung
+ * 
+ * Experimenting with movement
+ * 
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
