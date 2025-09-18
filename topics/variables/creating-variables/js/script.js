e5074a163cb1c8134052730c8a3148bcbfa4cecd
christie <christie.leung83@gmail.com>
2025-09-17 21:17:25 -0400

    started the creating variables module

---
+++ topics/variables/creating-variables/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Creating Variables
+ * Christie Leung
+ * 
+ * Experimenting with creating variables
+ */
+
+"use strict";
+
+/**
+ * Create a canvas
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
