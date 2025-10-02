008222cfa0a8e2f36bcf7cf5ceef385c2867d78d
christie <christie.leung83@gmail.com>
2025-10-02 08:44:50 -0400

    started the plain javascript events project

---
+++ topics/events/plain-javascript-events/js/script.js
@@ -0,0 +1,22 @@
+/**
+ * Plain Javascript Events
+ * Christie Leung
+ * 
+ * Experimenting with event handling in Plain Javascript
+ */
+
+"use strict";
+
+/**
+ * 
+*/
+function setup() {
+
+}
+
+/**
+ * 
+*/
+function draw() {
+
+}
