92c2c38e16ebebe7ba9f1ce202f007999224c2e0
christie <christie.leung83@gmail.com>
2025-10-02 07:24:32 -0400

    started the introducing events module

---
+++ topics/events/introducing-events/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Introducing events
+ * Christie Leung
+ * 
+ * Taking a look at how events work in Javascript and p5
+ * 
+ */
+
+"use strict";
+
+/**
+ * 
+*/
+function setup() {
+
+}
+
+
+/**
+ * 
+*/
+function draw() {
+
+}
