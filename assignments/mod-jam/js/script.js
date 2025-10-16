d3e52556bd895b4802733c2a3066efe55f32ea5a
christie <christie.leung83@gmail.com>
2025-10-16 17:27:33 -0400

    started the mod jam!

---
+++ assignments/mod-jam/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Mod Jam
+ * Christie Leung
+ * 
+ * 
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
