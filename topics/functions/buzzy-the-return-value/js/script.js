258341bd11dd4aa75a5bf64f9cf2cdf1e484658b
christie <christie.leung83@gmail.com>
2025-10-21 17:31:06 -0400

    created a createFly function

--- topics/functions/buzzy-the-return-value/js/script.js
+++ topics/functions/buzzy-the-return-value/js/script.js
@@ -8,38 +8,45 @@
 "use strict";
 
 // Our flies that will buzz around
-let buzzyTheFly = {
-    x: 200,
-    y: 200,
-    size: 20,
-    buzziness: 3
-};
-
-let jazzyTheFly = {
-    x: 200,
-    y: 200,
-    size: 20,
-    buzziness: 3
-};
+let buzzyTheFly = undefined;
+let jazzyTheFly = undefined;
+let jacuzziTheFly = undefined;
 
 /**
  * Create a canvas
  */
 function setup() {
     createCanvas(400, 400);
+    
+    buzzyTheFly = createFly(10);
+    jazzyTheFly = createFly(2);
+    jacuzziTheFly = createFly(5);
+    
+}
+
+function createFly(flyBuzziness) {
+    let fly = {
+        x: random(100, width - 100), // Dynamic
+        y: random(100, height - 100), // Dynamic
+        size: 20, // Default
+        buzziness: flyBuzziness // Parameter
+    }
+    return fly;
 }
 
 /**
- * Background, move and draw buzzy
+ * Background, move and draw flies
  */
 function draw() {
     background("#87ceeb");
 
     moveFly(buzzyTheFly);
     moveFly(jazzyTheFly);
+    moveFly(jacuzziTheFly);
 
     drawFly(buzzyTheFly);
     drawFly(jazzyTheFly);
+    drawFly(jacuzziTheFly);
 }
 
 /**
