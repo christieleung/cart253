8e867bf8e4db2f716ead2244a0a5383497fa2b43
christie <christie.leung83@gmail.com>
2025-11-06 14:57:21 -0500

    started gradient project

---
+++ topics/loops/gradient/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Gradient
+ * Christie Leung
+ * 
+ * Draws a gradient out of lines
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
