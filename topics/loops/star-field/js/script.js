0a0c3bcbfd761abdacff8b73f236b91cb263b4b2
christie <christie.leung83@gmail.com>
2025-11-06 14:55:46 -0500

    finished the star-field

--- topics/loops/star-field/js/script.js
+++ topics/loops/star-field/js/script.js
@@ -7,17 +7,39 @@
 
 "use strict";
 
+const numStars = 100;
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Create a canvas
 */
 function setup() {
-
+    createCanvas(400, 400);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws the star-field
 */
 function draw() {
+    background(0);
+    
+    randomSeed(1);
+    for (let i = 0; i < numStars; i++) {
+        drawStar();
+    }
+}
 
+/**
+ * Draws a random star
+ */
+function drawStar() {
+    const x = random(0, width);
+    const y = random(0, height);
+    const diameter = random(2, 5);
+    
+    push();
+    fill(255);
+    noStroke();
+    ellipse(x, y, diameter);
+    pop();
 }
