9d9cfe3bf7db0d5e8863faa9a45ffd6603693a1b
christie <christie.leung83@gmail.com>
2025-11-06 13:09:08 -0500

    started star-field project

---
+++ topics/loops/star-field/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Star-field
+ * Christie Leung
+ * 
+ * Draws a star-field with a for-loop!
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
