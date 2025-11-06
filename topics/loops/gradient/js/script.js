87a2e49fc92dfb25343cbb2b8b4436e5378c6db7
christie <christie.leung83@gmail.com>
2025-11-06 15:05:16 -0500

    finished the gradient project

--- topics/loops/gradient/js/script.js
+++ topics/loops/gradient/js/script.js
@@ -8,16 +8,29 @@
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates a canvas
 */
 function setup() {
-
+    createCanvas(600, 300);
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws a gradient
 */
 function draw() {
-
+    background(0);
+    
+    let y = 0;
+    
+    randomSeed(0);
+    for (let x = 0; x <= width; x += 3) {
+        const shade = map(x, 0, width, 0, 255);
+        
+        push();
+        stroke(shade);
+        line(x, y, x, height);
+        pop();
+        
+        y += random(0, 2);
+    }
 }
